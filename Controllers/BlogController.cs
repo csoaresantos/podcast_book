1e36345c78cd4e73877da66e0c4c7dbaec5ca602
Charles S <charlessoaresdossantos@gmail.com>
2020-02-02 22:32:46 -0300

    Created the post blog service api

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using podcast_book.Infra;
+using podcast_book.Repositories;
+
+namespace podcast_book.Controllers
+{
+    [Route("/api/[controller]")]
+    public class BlogController : ControllerBase
+    {
+        internal AppDatabase Db { get; }
+
+        public BlogController(AppDatabase appDatabase)
+        {
+            Db = appDatabase;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Db.Connection.OpenAsync();
+            var query = new BlogPostRepository(Db);
+            var result = query.LatestPostsAsync();
+            return new OkObjectResult(result);
+        }
+    }
+}
---
+++ Infra/AppDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace podcast_book.Infra
+{
+    public class AppDatabase : IDisposable
+    {
+        public MySqlConnection Connection { get; }
+
+        public AppDatabase(String connectionString)
+        {
+            Connection = new MySqlConnection("server=127.0.0.1;user id=root;password=;port=3306;Database=podcast_book;");
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
